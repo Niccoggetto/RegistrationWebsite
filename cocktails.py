0136a0e0ec9bc545dc7b04941bcf831a1c9daaec
Niccoggetto <129863214+Niccoggetto@users.noreply.github.com>
2024-02-08 23:37:34 +0100

    Add files via upload

--- cocktails.py
+++ cocktails.py
@@ -1,8 +1,10 @@
 class Cocktail:
-    def __init__(self, nome, alcolici, quantita_alcolici, analcolici, quantita_analcolici):
+    def __init__(self, nome, display_name, alcolici, quantita_alcolici, analcolici, quantita_analcolici, num):
         self.nome = nome
+        self.display = display_name
         self.alcohols = list(zip(alcolici[:2], quantita_alcolici[:2]))  # Coppia ogni alcolico con la quantità
         self.nonalcohols = list(zip(analcolici[:2], quantita_analcolici[:2]))  # Coppia ogni analcolico con la quantità
+        self.num = num
 
     '''def __str__(self):
         alcolici_str = ', '.join([f"{alcolico} ({quantita})" for alcolico, quantita in self.alcolici])
@@ -10,19 +12,16 @@ class Cocktail:
         return f"{self.nome}: Alcolici - {alcolici_str}, Analcolici - {analcolici_str}"'''
 
 
-vodka_lemon = Cocktail("vodka lemon", ["vodka"], [0.1], ["lemon"], [0.3])
-gin_tonic = Cocktail("gin Tonic", ["gin"], [0.2], ["tonic"], [0.4])
-gin_lemon = Cocktail("gin lemon", ["gin"], [0.2], ["lemon"], [0.3])
-rum_cola = Cocktail("rum e cola", ["rum"], [0.15], ["cola"], [0.35])
-spritz = Cocktail("spritz", ["prosecco", "aperol"], [0.2, 0.1], ["soda"], [0.3])
-tequila = Cocktail("tequila", ["tequila"], [0.1], [], [])
-prosecco = Cocktail("prosecco", ["prosecco"], [0.2], [], [])
-montenegro = Cocktail("montenegro", ["montenegro"], [0.15], [], [])
-amaro_del_capo = Cocktail("amaro del Capo", ["amarodelcapo"], [0.15], [], [])
-jagermeister = Cocktail("jagermeister", ["jagermaister"], [0.1], [], [])
+vodka_lemon = Cocktail("vodkalemon", "vodka lemon", ["vodka"], [0.1], ["lemon"], [0.3], 0)
+gin_tonic = Cocktail("gintonic", "gintonic", ["gin"], [0.2], ["tonic"], [0.4], 0)
+gin_lemon = Cocktail("ginlemon", "gin lemon", ["gin"], [0.2], ["lemon"], [0.3], 0)
+rum_cola = Cocktail("rumecola", "rum e cola", ["rum"], [0.15], ["cola"], [0.35], 0)
+spritz = Cocktail("spritz", "spritz", ["prosecco", "aperol"], [0.2, 0.1], ["soda"], [0.3], 0)
+tequila = Cocktail("tequila", "tequila", ["tequila"], [0.1], [], [], 0)
+prosecco = Cocktail("prosecco", "prosecco", ["prosecco"], [0.2], [], [], 0)
+montenegro = Cocktail("montenegro", "montenegro", ["montenegro"], [0.15], [], [], 0)
+amaro_del_capo = Cocktail("amarodelcapo", "amaro del capo", ["amarodelcapo"], [0.15], [], [], 0)
+jagermeister = Cocktail("jagermeister", "jagermeister", ["jagermaister"], [0.1], [], [], 0)
 
 cocktails_list = [vodka_lemon, gin_tonic, gin_lemon, rum_cola, spritz, tequila, prosecco, montenegro, amaro_del_capo, jagermeister]
-
-# Esempio di utilizzo
-print(vodka_lemon)
 
--- webmain.py
+++ webmain.py
@@ -1,11 +1,11 @@
 from db_connection import Database
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 from cocktails import Cocktail, cocktails_list
 
 db = Database()
 db.connect()
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
 
 
 @app.route('/')
@@ -13,19 +13,25 @@ def form():
     return render_template('registration.html', cocktails_list=cocktails_list)
 
 
-@app.route('/submit_form', methods=['POST'])
+@app.route('/submit_form', methods=['POST', 'GET'])
 def submit_form():
+    print(request.form)
     name = request.form['name']
     surname = request.form['surname']
     phone = request.form['phone']
     paymentchoice = request.form['paymentchoice']
 
     for c in cocktails_list:
-        cocktail_count = request.form.get(f'{c.nome}Count')
-        print(cocktail_count)
-        if cocktail_count != 0:
-            insert_cocktail_query = f"UPDATE alcohol SET quantity = quantity + %s WHERE name = {c.nome}"
+        '''cocktail_count = request.form[f'quantity_{c.nome}']
+        print(cocktail_count)'''         #problema quiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii
+        cocktail_name = f'quantity_{c.nome}'
+        print(f"Checking {cocktail_name}:", request.form.get(cocktail_name))
+        cocktail_count = request.form.get(cocktail_name)
+        if cocktail_count is not None:
+            cocktail_count = int(cocktail_count)
+            insert_cocktail_query = f"UPDATE alcohol SET quantity = quantity + %s WHERE name = '{c.nome}'"
             db.query(insert_cocktail_query, (cocktail_count,))
+            print("done")
 
     insert_data_query = "INSERT INTO people (name, surname, phone, payment) VALUES (%s, %s, %s, %s)"
     people_data = (name, surname, str(phone), paymentchoice)
